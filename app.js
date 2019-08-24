910418fcdc11fbafc8dfbb5cd92eeabf832f6677
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-24 17:09:02 +0530

    viewBatsmen &viewBowlers set up. addBatsmen & addBowlers set up.

--- app.js
+++ app.js
@@ -13,18 +13,26 @@ navlink = [
     {
         "title":"Batsmen",
         "link":"/viewBatsmen"
+    },{
+        "title":"Bowlers",
+        "link":"/viewBowlers"
+    },
+    {
+        "title":"Add player",
+        "link":"/addPlayer"
     }
-]
+];
 
 Mongoose.connect('mongodb://localhost:27017/cricketDB',{ useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     //console.log('Database online');
     });
 
-/////////////////////////
-//Define dataschemas here
+//-----------Define dataschemas here--------------
+
 const batsmenSchema = Mongoose.model('batsmans',{
     name:String,
+    urating:String,
     status:String,
     country:String,
     role:String,
@@ -50,6 +58,7 @@ const batsmenSchema = Mongoose.model('batsmans',{
 
 const bowlersSchema = Mongoose.model('bowlers',{
     name:String,
+    urating:String,
     status:String,
     country:String,
     style:String,
@@ -59,7 +68,7 @@ const bowlersSchema = Mongoose.model('bowlers',{
     tballs:String,
     odiballs:String,
     t20balls:String,
-    wkts:String,
+    twkts:String,
     odiwkts:String,
     t20wkts:String,
     trate:String,
@@ -71,19 +80,20 @@ const bowlersSchema = Mongoose.model('bowlers',{
     imgsrc:String,
     profile:String
 });
-//////////////////////////
+//-------------------------------
 //save batsmen entriesAPI
-// app.post('/saveBatsman', (req,res)=>{
-//     var item = req.body;
-//     var batsman = batsmenSchema(item);
-//     batsman.save((error,data)=>{
-//         if(error){
-//             throw error;
-//         }else{
-//             res.send('new object created @' + data);
-//         }
-//     })
-// });
+app.post('/saveBatsman', (req,res)=>{
+    var item = req.body;
+    var batsman = batsmenSchema(item);
+    batsman.save((error,data)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send('new object created @' + data);
+        }
+    })
+});
+//-----Retrieve All Batsmen---------------
 
 //retrieveallbatsmenAPI
 app.get('/retrieveBatsmenAPI',(req,res)=>{
@@ -100,7 +110,7 @@ app.get('/retrieveBatsmenAPI',(req,res)=>{
 //Retrieve Batsmen APILInk
 const retrieveBatsmenAPILink = 'http://localhost:3046/retrieveBatsmenAPI'
 
-//retrieveallbatsmenfunctioncall
+//retrieveallbatsmen set view
 app.get('/',(req,res)=>{
     request(retrieveBatsmenAPILink,(error,response,body)=>{
         if (error){
@@ -108,7 +118,6 @@ app.get('/',(req,res)=>{
             res.send(error);
         }else {
             data = JSON.parse(body);
-            console.log(data);
             res.render('viewBatsmen',{nav:navlink, title: "Batsmen", batsmans:data })
         }
     })
@@ -120,16 +129,69 @@ app.get('/viewBatsmen',(req,res)=>{
             throw error;
             res.send(error);
         }else {
-            data = JSON.parse(body);
-            console.log(data);
+            var data = JSON.parse(body);
             res.render('viewBatsmen',{nav:navlink, title: "Batsmen", batsmans:data })
         }
     })
 });
-// app.get('/',(req,res)=>{
-//     res.render('viewBatsmen',{nav:navlink,title:"Batsmen"});
-// });
+//-------------------------------------------
+//--------Save Bowlers-------------------
+//save batsmen entriesAPI
+app.post('/saveBowler', (req,res)=>{
+    var item = req.body;
+    var bowler = bowlersSchema(item);
+    bowler.save((error,data)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send('new object created @' + data);
+        }
+    })
+});
+//--------Retrive Bowlers--------------------
+//RetriveBowlersAPI
+app.get('/retrieveBowlersAPI',(req, res)=>{
+    bowlersSchema.find((error,data)=>{
+        if (error){
+            throw error;
+            res.send(data);
+        }else{
+            res.send(data);
+        }
+    });
+});
 
+//RetrieveBowlersAPILink
+const retrieveBowlersAPILink = 'http://localhost:3046/retrieveBowlersAPI'
+
+//RetrieveBowlersfunction
+app.get('/viewBowlers',(req,res)=>{
+    request(retrieveBowlersAPILink,(error,response,body)=>{
+        if(error){
+            throw error;
+            res.send(error);
+        }else{
+            var data = JSON.parse(body);
+            res.render('viewBowlers',{nav:navlink, title:'Bowlers', bowlers:data})
+        }
+    });
+});
+//---------------------------------------
+//-------Add players to database--------------
+
+//set selection view
+app.get('/addPlayer',(req,res)=>{
+    res.render('addPlayer',{nav:navlink, title:"Add Player"});
+});
+
+app.get('/addBatsman',(req,res)=>{
+    res.render('addBatsman',{nav:navlink, title:"Add Batsman"});
+});
+
+app.get('/addBowler',(req,res)=>{
+    res.render('addBowler',{nav:navlink, title:"Add Bowler"});
+})
+//---------------------------------------
 app.listen(process.env.PORT || 3046,()=>{
     console.log("Server running at http://localhost:3046")
 });
